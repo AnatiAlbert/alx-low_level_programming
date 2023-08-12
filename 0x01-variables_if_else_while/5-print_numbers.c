5dba0ba9c684aef900f842fd794192b470822fc6
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-12 11:33:40 -0700

    Program prints single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program prints all single numbers of base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
