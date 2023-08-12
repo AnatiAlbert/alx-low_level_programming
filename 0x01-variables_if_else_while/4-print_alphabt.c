1644bf81456851697310055598500ae7ed51500c
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-12 09:57:58 -0700

    Program prints lower case except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program tha prints alphabet in lower expt q and e
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if ((alphabet != 'q') && (alphabet != 'e'))
+		{
+		putchar(alphabet);
+		alphabet++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
