a97f92f36b8e20053477404f79aebef033041edd
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-13 03:44:36 -0700

    Program prints all combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,14 @@ int main(void)
 	int num;
 	int num1;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 8; num++)
 	{
-		for (num1 = 0; num1 <= 9; num1++)
+		for (num1 = num + 1; num1 <= 9; num1++)
 		{
-			if (num != num1)
-			{
-				putchar(num + '0');
-				putchar(num1 + '0');
-			}
+			putchar(num + '0');
+			putchar(num1 + '0');
 
-			if (num < 8 || num1 < 9)
+			if (num != 8 || num1 != 9)
 			{
 				putchar(',');
 				putchar(' ');
