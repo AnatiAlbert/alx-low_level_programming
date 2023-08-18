626a4296f11111a8193122fa303854b649770286
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-18 14:51:47 -0700

    function prints largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - program prints prime factors of a number.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long number = 612852475143;
+	int prime;
+	long largest = 0;
+
+	for  (prime = 2; prime > 1; prime++)
+	{
+		while (number % prime == 0)
+		{
+			number = number / prime;
+		}
+		if (largest < prime)
+		{
+			largest = prime;
+		}	
+	}
+	printf("The largest is %ld\n", largest);
+	return (0);
+}
