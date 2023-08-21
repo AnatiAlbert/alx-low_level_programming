4cf3f38007a47ef231a9ac5d8c1ba9e15bcc4bd0
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-21 09:39:52 -0700

    update value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of int to 98
+ * @n: value to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains files about pointers, arrays and strings.
