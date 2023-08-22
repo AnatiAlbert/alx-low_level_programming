d8cdf5a57427f8a5b975651ff53e977a7493366c
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-22 10:37:42 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,13 @@ void rev_string(char *s)
 {
 	int len = strlen(s);
 	int c1;
+	int c2;
 
-	for (c1 = len - 1; c1 >= 0; c1--)
+	for (c1 = 0, c2 = len - 1; c1 < c2; c1++, c2--)
 	{
-		_putchar(s[c1]);
+		char word = s[c1];
+
+		s[c1] = s[c2];
+		s[c2] = word;
 	}
 }
