b6a0c112f0477bcd03e9ef1463e548873328f878
AnatiAlbert <anatiprogrammer@gmail.com>
2023-08-21 21:52:49 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverse a string
+ * @s: target string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int c1;
+
+	for (c1 = len - 1; c1 >= 0; c1--)
+	{
+		putchar(s[c1]);
+	}
+	putchar('\n');
+}
