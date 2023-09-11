0e6770091cb3c912b313c6a71f455d39674a3104
AnatiAlbert <anatiprogrammer@gmail.com>
2023-09-11 09:59:01 -0700

    structure about a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains code files of structures in C programming.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - describes elements for dog
+ * @name: holds name of dog
+ * @age: shows age of dog
+ * @owner: shows owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
