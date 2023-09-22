f3e25b117cdcf00ecd4728cfc8a636d062d631dc
AnatiAlbert <anatiprogrammer@gmail.com>
2023-09-22 09:38:45 -0700

    print all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t st = 0;
+	size_t stg = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		st++;
+		stg++;
 	}
 
-	return (st);
+	return (stg);
 }
