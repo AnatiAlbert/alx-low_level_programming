6462093425fffc4567b1e239a31f452ab570b50b
AnatiAlbert <anatiprogrammer@gmail.com>
2023-10-31 03:04:04 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	for (nl = 0; text_content(nl); nl++)
+	for (nl = 0; text_content[nl]; nl++)
 		;
 	rw = write(fild, text_content, nl);
 	if (rw == -1)
